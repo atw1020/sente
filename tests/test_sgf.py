41820936ab777568c68a9c0e5f928d8e59ce239a
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2021-07-05 12:34:21 -0700

    added loads functionality

--- tests/test_sgf.py
+++ tests/test_sgf.py
@@ -23,7 +23,7 @@ class BasicSGF(DoesNotRaiseTestCase):
 
         files = os.listdir("sgf")
 
-        # sgf.load(os.path.join("sgf", "34839594-255-IDW64-noob_bot_3.sgf"))
+        # sgf.load(os.path.join("sgf", "extra letter in move.sgf"))
 
         for file in files:
             with self.assertDoesNotRaise(Exception):
@@ -106,6 +106,114 @@ class BasicSGF(DoesNotRaiseTestCase):
         self.assertEqual(expected_game, game.get_board())
 
 
+class StringLoad(DoesNotRaiseTestCase):
+    """
+
+    tests to see if SGFs can be loaded from strings and not just from files
+
+    """
+
+    def test_all_sgfs(self):
+        """
+
+        tests to see if any of the sgf files in the directory raise any exceptions
+
+        :return:
+        """
+
+        files = os.listdir("sgf")
+
+        # sgf.load(os.path.join("sgf", "extra letter in move.sgf"))
+
+        for file in files:
+            with self.assertDoesNotRaise(Exception):
+                with open(os.path.join("sgf", file)) as sgf_file:
+                    game = sgf.loads(sgf_file.read())
+                    game.play_default_branch()
+
+    def test_simple_single_branch_file(self):
+        """
+
+        tests to see if we can import a sgf file with a single branch
+
+        :return:
+        """
+
+        B = BLACK
+        W = WHITE
+        _ = EMPTY
+
+        with open("sgf/simple_sequence.sgf") as sgf_file:
+            game = sgf.loads(sgf_file.read())
+        game.play_default_branch()
+
+        expected_game = Board19([[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, B, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+                                 [_, _, _, B, _, _, _, _, _, _, _, _, _, _, _, _, B, W, W],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, W, _, _],
+                                 [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, W, W]])
+
+        self.assertEqual(expected_game, game.get_board())
+
+    def test_full_single_branch_game(self):
+        """
+
+        tests to see if the module can keep track of an entire game
+
+        :return:
+        """
+
+        B = BLACK
+        W = WHITE
+        _ = EMPTY
+
+        with open("sgf/34839594-255-IDW64-noob_bot_3.sgf") as sgf_file:
+            game = sgf.loads(sgf_file.read())
+        game.play_default_branch()
+
+        expected_game = Board19([[_, _, _, _, _, _, B, W, W, W, W, W, W, B, _, _, _, _, _],
+                                 [_, _, _, _, W, B, B, B, W, B, B, B, W, B, _, _, _, _, _],
+                                 [_, _, W, _, W, W, B, B, B, _, B, B, W, B, B, _, W, _, _],
+                                 [_, _, _, W, _, _, W, B, B, _, B, W, W, W, B, B, W, _, _],
+                                 [W, W, W, B, W, W, W, W, B, W, W, _, W, W, B, _, _, _, _],
+                                 [B, B, B, B, B, W, B, B, W, _, W, _, _, _, W, W, _, _, _],
+                                 [_, _, _, _, B, W, B, B, W, _, W, W, W, W, _, W, _, _, _],
+                                 [W, W, W, W, W, B, B, W, W, _, _, _, W, _, W, W, B, _, _],
+                                 [B, B, B, W, B, B, B, B, W, _, _, W, B, B, _, _, _, _, _],
+                                 [_, _, B, W, W, B, _, B, W, _, _, W, _, _, _, W, W, W, _],
+                                 [_, _, B, B, W, B, _, W, _, _, _, W, _, B, B, W, B, W, W],
+                                 [W, W, B, B, W, B, _, W, _, _, W, B, B, B, W, B, B, B, W],
+                                 [B, W, W, B, W, B, B, B, W, _, _, _, _, B, W, _, B, B, W],
+                                 [B, _, _, W, W, B, B, B, W, _, _, W, W, W, W, _, _, _, B],
+                                 [B, B, B, B, B, W, W, W, _, _, W, W, B, W, _, W, B, B, B],
+                                 [W, W, W, W, W, W, B, B, W, W, B, B, B, B, B, W, W, W, W],
+                                 [B, B, B, W, B, _, _, B, B, B, _, B, _, B, _, W, B, _, _],
+                                 [B, B, _, W, B, B, B, _, _, _, B, W, B, _, _, W, B, B, B],
+                                 [_, B, _, W, W, B, _, W, W, _, W, W, W, W, _, W, B, B, _]])
+
+        self.assertEqual(str(expected_game), str(game))
+        self.assertEqual(expected_game, game.get_board())
+
+
+class Serialization(TestCase):
+
+    pass
+
+
 class BranchedSGF(TestCase):
 
     def test_simple_branched_sgf(self):
@@ -161,6 +269,8 @@ class InvalidSGF(TestCase):
 
         :return:
         """
+
+        self.assertRaises()
 
     def test_nonexistent_file(self):
         """
