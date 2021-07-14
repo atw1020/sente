358b983c9782b29373b1d5f1ff464bc6bc7819ec
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2021-07-13 21:26:59 -0700

    switch to cloud theme

--- docs/conf.py
+++ docs/conf.py
@@ -21,12 +21,12 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'sente'
+project = 'Sente'
 copyright = '2021, Arthur Wesley'
 author = 'Arthur Wesley'
 
 # The full version, including alpha/beta/rc tags
-release = 'alpha-0.1.0'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -51,7 +51,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'cloud'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/index.rst
+++ docs/index.rst
@@ -1,9 +1,9 @@
-.. sente documentation master file, created by
+.. Sente documentation master file, created by
    sphinx-quickstart on Tue Jul 13 14:00:48 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to sente's documentation!
+Welcome to Sente's documentation!
 =================================
 
 .. toctree::
