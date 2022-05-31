dea97db600cd759d3ae5cb6b29b8936a8db66ad9
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2022-05-31 14:17:05 -0700

    switch from CMake backend to Meson

--- setup.py
+++ setup.py
@@ -4,116 +4,53 @@ Author: Arthur Wesley
 
 """
 
-import os
-import re
 import sys
+import shutil
 import subprocess
+from pathlib import Path
 
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
-# Convert distutils Windows platform specifiers to CMake -A arguments
-PLAT_TO_CMAKE = {
-    "win32": "Win32",
-    "win-amd64": "x64",
-    "win-arm32": "ARM",
-    "win-arm64": "ARM64",
-}
 
-
-# A CMakeExtension needs a sourcedir instead of a file list.
-# The name must be the _single_ output extension from the CMake build.
-# If you need multiple extensions, see scikit-build.
-class CMakeExtension(Extension):
+class MesonExtension(Extension):
     def __init__(self, name, sourcedir=""):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
+        Extension.__init__(self, name=name, sources=[])
+        self.sourcedir = Path(sourcedir).absolute()
 
 
-class CMakeBuild(build_ext):
-    def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+class MesonBuild(build_ext):
+    def build_extension(self, ext) -> None:
 
-        # required for auto-detection of auxiliary "native" libs
-        if not extdir.endswith(os.path.sep):
-            extdir += os.path.sep
+        # obtain the name of the output file and the output path
+        ext_filename = Path(self.get_ext_filename(ext.name))
+        ext_fullpath = Path(self.get_ext_fullpath(ext.name)).parent
 
-        cfg = "Debug" if self.debug else "Release"
+        # initialize the extension directory and temporary directory
+        temp_dir = Path(self.build_temp)
 
-        # CMake lets you override the generator - we need to check this.
-        # Can be set with Conda-Build, for example.
-        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+        # create the temporary directory if it does not exist
+        if not temp_dir.exists():
+            # create the build directory
+            subprocess.check_call(
+                ["meson", "setup", temp_dir]
+            )
 
-        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
-        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
-        # from Python.
-        cmake_args = [
-            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(extdir),
-            "-DPYTHON_EXECUTABLE={}".format(sys.executable),
-            "-DEXAMPLE_VERSION_INFO={}".format(self.distribution.get_version()),
-            "-DCMAKE_BUILD_TYPE={}".format(cfg),  # not used on MSVC, but no harm
-        ]
-        build_args = []
+            # configure meson for the correct python version
+            subprocess.check_call(
+                ["meson", "configure", temp_dir, "-Dpython_executable=" + sys.executable]
+            )
 
-        if self.compiler.compiler_type != "msvc":
-            # Using Ninja-build since it a) is available as a wheel and b)
-            # multithreads automatically. MSVC would require all variables be
-            # exported for Ninja to pick it up, which is a little tricky to do.
-            # Users can override the generator with CMAKE_GENERATOR in CMake
-            # 3.15+.
-            if not cmake_generator:
-                try:
-                    import ninja  # noqa: F401
-
-                    cmake_args += ["-GNinja"]
-                except ImportError:
-                    pass
-
-        else:
-
-            # Single config generators are handled "normally"
-            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
-
-            # CMake allows an arch-in-generator style for backward compatibility
-            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
-
-            # Specify the arch if using MSVC generator, but only if it doesn't
-            # contain a backward-compatibility arch spec already in the
-            # generator name.
-            if not single_config and not contains_arch:
-                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
-
-            # Multi-config generators have a different way to specify configs
-            if not single_config:
-                cmake_args += [
-                    "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
-                ]
-                build_args += ["--config", cfg]
-
-        if sys.platform.startswith("darwin"):
-            # Cross-compile support for macOS - respect ARCHFLAGS if set
-            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
-            if archs:
-                cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
-
-        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
-        # across all generators.
-        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
-            # self.parallel is a Python 3 only way to set parallel jobs by hand
-            # using -j in the build_ext call, not supported by pip or PyPA-build.
-            if hasattr(self, "parallel") and self.parallel:
-                # CMake 3.12+ only.
-                build_args += ["-j{}".format(self.parallel)]
-
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-
+        # compile the code
         subprocess.check_call(
-            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
+            ["meson", "compile", "-C", self.build_temp],
         )
-        subprocess.check_call(
-            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
-        )
+
+        if not ext_fullpath.exists():
+            ext_fullpath.mkdir(parents=True, exist_ok=True)
+
+        # copy the executable
+        shutil.copy(temp_dir/ext_filename, ext_fullpath/ext_filename)
 
 
 def read_file(filename):
@@ -149,8 +86,8 @@ setup(
                  "Operating System :: POSIX :: Linux",
                  "Operating System :: Microsoft :: Windows :: Windows 10"],
     author_email="arthur@electricfish.com",
-    ext_modules=[CMakeExtension("sente")],
+    ext_modules=[MesonExtension("sente")],
     test_suite="tests",
-    cmdclass={"build_ext": CMakeBuild},
+    cmdclass={"build_ext": MesonBuild},
     zip_safe=False
 )
